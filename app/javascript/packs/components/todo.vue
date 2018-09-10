5d539b858a81471dc35da3e0a8d87fe185528871
Andrew <thesimpledev@gmail.com>
2018-09-10 11:03:49 -0700

    fix javascript todaysDate by + 1, since it starts at 0

--- app/javascript/packs/components/todo.vue
+++ app/javascript/packs/components/todo.vue
@@ -157,7 +157,7 @@ export default{
     
     todaysDate() {
       let date = new Date()
-      return `${date.getFullYear()}-${("0" + date.getMonth()).slice(-2)}-${("0" + date.getDate()).slice(-2)}`
+      return `${date.getFullYear()}-${("0" + (date.getMonth() + 1)).slice(-2)}-${("0" + date.getDate()).slice(-2)}`
     }
   },
 
